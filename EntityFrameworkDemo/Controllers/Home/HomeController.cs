29d2797ea443eae3c5e0dab8fac0254624df966c
谢怡彤 <yt.xie@partner.midea.com.cn>
2016-10-25 10:16:41 +0800

    提交版本

---
+++ EntityFrameworkDemo/Controllers/Home/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegisterUser(FormCollection form)
+        {
+            string a = form["username"];
+            string b = form["email"];
+            //string a = Request["username"];
+            //string b = Request["email"];
+            return View();
+        }
+
+        public JsonResult GetPageData(string username, string password, string email, string repassword)
+        {
+            var result = "";// userinfoDataAccessors.CheckEmailIsexist(email);
+            var json = Json(new
+            {
+                state = result
+            }, JsonRequestBehavior.DenyGet);
+            return json;
+        }
+
+        public JsonResult GetPassword(string email)
+        {
+
+            
+            var result = EntityFrameworkDemo.DataAccess.SendEmail.SendEmailByUser(email);// userinfoDataAccessors.CheckEmailIsexist(email);
+            var json = Json(new
+            {
+                state = result
+            }, JsonRequestBehavior.DenyGet);
+            return json;
+        }
+        //方法1:使用传统的Request请求取值
+        //[HttpPost]
+        //public ActionResult AddNews()
+        //{
+        //    string a = Request["text1"];
+        //    string b = Request["text2"];
+        //}
+        //方法2:Action参数名与表单元素name值一一对应
+        //[HttpPost]
+        //public ActionResult AddNews(string text1, string text2)
+        //{
+        //    string a = text1;
+        //    string b = text2;
+        //}
+        //方法3:从MVC封装的FormCollection容器中读取
+        //[HttpPost]
+        //public ActionResult AddNews(FormCollection form)
+        //{
+        //    string a = form["text1"];
+        //    string b = form["text2"];
+        //}
+        //方法4:使用实体作为Action参数传入，前提是提交的表单元素名称与实体属性名称一一对应
+        //[HttpPost]
+        //public ActionResult AddNews(userModel user)
+        //{
+        //    string a = user.text1;
+        //    string b = user.text2;
+        //}
+
+
+        public ActionResult Widgets()
+        {
+            return View();
+        }
+
+        public ActionResult ChartJs()
+        {
+            return View();
+        }
+
+        public ActionResult Morris()
+        {
+            return View();
+        }
+
+        public ActionResult Flot()
+        {
+            return View();
+        }
+
+        public ActionResult Inline()
+        {
+            return View();
+        }
+    }
+}
---
+++ EntityFrameworkDemo/DataAccess/SendEmail.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace EntityFrameworkDemo.DataAccess
+{
+    public class SendEmail
+    {
+        public static string SendEmailByUser(string email)
+        {
+            SmtpClient mailClient = new SmtpClient("smtp.163.com");
+            //Credentials登陆SMTP服务器的身份验证.
+            mailClient.Credentials = new NetworkCredential("18109176085", "xyt13636765500");
+            //test@qq.com发件人地址、test@tom.com收件人地址
+            MailMessage message = new MailMessage(new MailAddress("18109176085@163.com"), new MailAddress(email));
+
+            // message.Bcc.Add(new MailAddress("tst@qq.com")); //可以添加多个收件人
+            message.Body = "你好，你的密码修改为123456，请及时修改";//邮件内容
+            message.Subject = "修改密码";//邮件主题
+                                          //Attachment 附件
+                                          //Attachment att = new Attachment(@"C:/hello.txt");
+                                          //message.Attachments.Add(att);//添加附件
+                                          //Console.WriteLine("Start Send Mail....");
+                                          //发送....
+            try
+            {
+                mailClient.Send(message);
+            }
+            catch (Exception e)
+            {
+
+                return "发送失败" + e;
+            }
+            //Console.WriteLine("Send Mail Successed");
+            //Console.ReadLine();
+            return "发送成功！";
+        }
+    }
+}
